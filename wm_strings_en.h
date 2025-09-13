335ee519450608c78ea374de8f8a5fe63ab8c320
hirdle <admin@hirdle.ru>
2025-09-13 22:28:10 +0300

    one fix

--- wm_strings_en.h
+++ wm_strings_en.h
@@ -72,7 +72,7 @@ const char HTTP_FORM_PARAM_HEAD[]  PROGMEM = "<hr><br/>";
 const char HTTP_FORM_PARAM[]       PROGMEM = "<br/><input id='{i}' name='{n}' maxlength='{l}' value='{v}' {c}>\n"; // do not remove newline!
 
 const char HTTP_SCAN_LINK[]        PROGMEM = "<br/><form action='/wifi?refresh=1' method='POST'><button name='refresh' value='1'>Обновить</button></form>";
-const char HTTP_SAVED[]            PROGMEM = "<div class='msg'>Сохранение данных<br/>Попытка подключения ESP к сети.<br />Если не удастся, переподключитесь к точке доступа, чтобы попробовать снова</div>";
+const char HTTP_SAVED[]            PROGMEM = "<div class='msg'>Сохранение данных<br/>Попытка подключения устройства к сети.<br />Если не удастся, переподключитесь к точке доступа, чтобы попробовать снова</div>";
 const char HTTP_PARAMSAVED[]       PROGMEM = "<div class='msg S'>Сохранено<br/></div>";
 const char HTTP_END[]              PROGMEM = "</div></body></html>";
 const char HTTP_ERASEBTN[]         PROGMEM = "<br/><form action='/erase' method='get'><button class='D'>Удалить конфигурацию WiFi</button></form>";
